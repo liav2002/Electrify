4f85373785358f597318ca6464e32b5028cc0138
liav2002 <liavmail00@gmail.com>
2022-07-12 18:27:13 +0300

    implementation of battery progress bar, and get the capacity from the database.

--- electrifyWebApp.py
+++ electrifyWebApp.py
@@ -1,36 +1,39 @@
 from flask import Flask, render_template, url_for, flash, redirect
+
+from battery import *
 from forms import RegistrationForm, LoginForm
-from sql import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '584107ac33a499cb87847a6265f3bc1be'
-app.config['DATABASE'] = 'ElectrifyDataBase.sqlite'
 
-global isLoggedIn
-isLoggedIn = False
+user_id = 0
 
 
 @app.route("/")
 def home():
-    return render_template('home.html', isLoggedIn=isLoggedIn)
+    return render_template('home.html', isLoggedIn=user_id != 0)
 
 
 @app.route("/system")
 def system():
-    return render_template('system.html', isLoggedIn=isLoggedIn)
+    if user_id != 0:
+        return render_template('system.html', isLoggedIn=user_id != 0, battery=get_battery_capacity(user_id))
+
+    else:
+        return redirect(url_for('home'))
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        if add_user(app.config['DATABASE'], form.id.data, form.username.data, form.email.data, form.password.data,
-                    form.phone.data, form.first_name.data, form.last_name.data) and add_credit(app.config['DATABASE'],
-                                                                                               form.id.data,
-                                                                                               form.c_number.data,
-                                                                                               form.cvv.data,
-                                                                                               form.c_month.data,
-                                                                                               form.c_year.data):
+        if add_user(form.id.data, form.username.data, form.email.data, form.password.data,
+                    form.phone.data, form.first_name.data, form.last_name.data) \
+                and add_credit(form.id.data,
+                               form.c_number.data,
+                               form.cvv.data,
+                               form.c_month.data,
+                               form.c_year.data) and generate_battery(form.id.data):
             flash(f'Account created for {form.username.data}!', 'success')
             return redirect(url_for('home'))
         else:
@@ -42,10 +45,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if check_login_fields(app.config['DATABASE'], form.email.data, form.password.data):
+        if check_login_fields(form.email.data, form.password.data):
             flash('You have been logged in!', 'success')
-            global isLoggedIn
-            isLoggedIn = True
+            global user_id
+            user_id = get_user_id_by_email(form.email.data)
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
@@ -56,8 +59,8 @@ def login():
 def logout():
     flash('You have been logged out', 'success')
 
-    global isLoggedIn
-    isLoggedIn = False
+    global user_id
+    user_id = 0
 
     return redirect(url_for('home'))
 
--- sql.py
+++ sql.py
@@ -1,24 +1,26 @@
 import sqlite3
 from sqlite3 import Error
 
+db_file = 'ElectrifyDataBase.sqlite'
 
-def create_connection(db_file):
+
+def create_connection():
     """ create a database connection to a SQLite database """
 
     try:
         sqlHandler = sqlite3.connect(db_file)
         return sqlHandler
     except Error as e:
-        print(e)
+        print("ERROR:create_connection: " + str(e))
 
 
-def check_login_fields(db_file, email, password):
+def check_login_fields(email, password):
     """ Checks whether the user is registered to the system and whether the password is correct """
 
     status = True
 
     try:
-        sqlHandler = create_connection(db_file)
+        sqlHandler = create_connection()
 
         query = f"SELECT COUNT(*) FROM Users WHERE Email = '{email}' AND Password = '{password}';"
         cursor = sqlHandler.execute(query)
@@ -29,19 +31,19 @@ def check_login_fields(db_file, email, password):
         sqlHandler.close()
 
     except Exception as e:
-        print(e)
+        print("ERROR:check_login_fields: " + str(e))
         status = False
 
     return status
 
 
-def add_user(db_file, id, username, email, password, phone, first_name, last_name):
+def add_user(id, username, email, password, phone, first_name, last_name):
     """ Add new User to Users table """
 
     status = True
 
     try:
-        sqlHandler = create_connection(db_file)
+        sqlHandler = create_connection()
 
         query = f"INSERT INTO Users (ID, Username, Email, Password, Phone, FirstName, LastName) " \
                 f"VALUES ({id}, '{username}', '{email}', '{password}', '{phone}', '{first_name}', '{last_name}');"
@@ -51,19 +53,19 @@ def add_user(db_file, id, username, email, password, phone, first_name, last_nam
         sqlHandler.close()
 
     except Error as e:
-        print(e)
+        print("ERROR:add_user: " + str(e))
         status = False
 
     return status
 
 
-def add_credit(db_file, user_id, c_number, cvv, c_month, c_year):
+def add_credit(user_id, c_number, cvv, c_month, c_year):
     """ Add new Credit to Credit table """
 
     status = True
 
     try:
-        sqlHandler = create_connection(db_file)
+        sqlHandler = create_connection()
 
         query = f"INSERT INTO Credits (UserID, C_Number, CVV, C_Month, C_Year)" \
                 f"VALUES ({user_id}, '{c_number}', {cvv}, '{c_month}', {c_year});"
@@ -73,19 +75,19 @@ def add_credit(db_file, user_id, c_number, cvv, c_month, c_year):
         sqlHandler.close()
 
     except Error as e:
-        print(e)
+        print("ERROR:add_credit: " + str(e))
         status = False
 
     return status
 
 
-def add_battery(db_file, id, user_id, capacity, consumption):
+def add_battery(id, user_id, capacity, consumption):
     """ Add new Battery to Batteries table """
 
     status = True
 
     try:
-        sqlHandler = create_connection(db_file)
+        sqlHandler = create_connection()
 
         query = f"INSERT INTO Batteries (ID, UserID, Capacity, Consumption)" \
                 f"VALUES ({id}, {user_id}, {capacity}, {consumption});"
@@ -95,7 +97,59 @@ def add_battery(db_file, id, user_id, capacity, consumption):
         sqlHandler.close()
 
     except Error as e:
-        print(e)
+        print("ERROR:add_battery: " + str(e))
         status = False
 
     return status
+
+
+def get_number_of_batteries():
+    try:
+        sqlHandler = create_connection()
+
+        query = f"SELECT COUNT(*) FROM Batteries;"
+        cursor = sqlHandler.execute(query)
+        result = cursor.fetchone()[0]
+
+        sqlHandler.commit()
+        sqlHandler.close()
+
+    except Error as e:
+        print("ERROR:get_number_of_batteries: " + str(e))
+        result = -1
+
+    return result
+
+
+def get_user_id_by_email(email):
+    try:
+        sqlHandler = create_connection()
+        query = f"SELECT ID FROM Users WHERE Email='{email}';"
+        cursor = sqlHandler.execute(query)
+        rows = cursor.fetchall()
+
+        if len(rows) != 0:
+            return int(rows[0][0])
+        else:
+            return 0
+
+    except Error as e:
+        print("ERROR:get_user_id_by_email: " + str(e))
+        return 0
+
+
+def get_battery_capacity(user_id):
+    try:
+        sqlHandler = create_connection()
+        query = f"SELECT Capacity FROM Batteries WHERE UserID={user_id};"
+        cursor = sqlHandler.execute(query)
+        rows = cursor.fetchall()
+
+        if len(rows) != 0:
+            return float(rows[0][0])
+        else:
+            return -1
+
+    except Error as e:
+        print("ERROR:get_battery_capacity: " + str(e))
+        return -1
