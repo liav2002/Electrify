912d7183ece7bce78aa43bf9862369490fb0cb40
liav2002 <liavmail00@gmail.com>
2022-10-29 21:06:41 +0300

    add function that measures voltage using gpiozero libary.

--- temp.py
+++ temp.py
@@ -1,26 +1,33 @@
 from sql import *
 import random
 import datetime
-import time
+from time import sleep
+from gpiozero import MCP3008
 
-id = 6
+sample_id = 6
+vref = 3.3
 
 
-def fill_samples():
+def fill_samples(user_id):
     while (True):
-        sample = sampling(12345677)
+        sample = sampling(user_id)
         add_sample(sample[0], sample[1], sample[2], sample[3], sample[4])
         print(f"add sample: {sample[0]}|{sample[1]}|{sample[2]}|{sample[3]}|{sample[4]}")
-        time.sleep(2.4)  # sleep 2.4 seconds
+        sleep(0.1)  # sleep 2.4 seconds
 
 
 def sampling(user_id):
-    global id
-    sample = [id, user_id, datetime.datetime.now(), round(random.uniform(0, 4.2), 2),
-              round(random.uniform(0, 100.0), 2)]
-    id += 1
+    global sample_id
+    sample = [sample_id, user_id, datetime.datetime.now(), round(random.uniform(0, 4.2), 2),
+              getVoltage(0)]
+    sample_id += 1
 
     return sample
 
 
-fill_samples()
+def getVoltage(channel):
+    with MCP3008(channel=channel) as adc:
+        return adc.value * vref
+
+
+fill_samples(1234567)
